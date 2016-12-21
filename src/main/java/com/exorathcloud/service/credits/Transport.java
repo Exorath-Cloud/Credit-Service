7a6271ca0ecf4dd82a202a38045489254d2d7c30
Toon Sevrin <twan123@live.be>
2016-12-21 23:31:42 +0100

    Added json over http transport

--- src/main/java/com/exorathcloud/service/credits/Transport.java
+++ src/main/java/com/exorathcloud/service/credits/Transport.java
@@ -1,14 +1,58 @@
 package com.exorathcloud.service.credits;
 
+
+import com.exorath.service.commons.portProvider.PortProvider;
+import com.exorathcloud.service.credits.res.Transaction;
+import com.exorathcloud.service.credits.res.TransactionState;
+import com.google.gson.Gson;
+import spark.Route;
+
+import java.util.Calendar;
+
+import static spark.Spark.get;
+import static spark.Spark.patch;
+import static spark.Spark.port;
+
 /**
  * Created by toonsev on 12/17/2016.
  */
 public class Transport {
+    private static final Gson GSON = new Gson();
+
     /**
      * Sets up the http transport to the service
+     *
      * @param service service instance
      */
-    public static void setup(Service service){
+    public static void setup(Service service, PortProvider portProvider) {
+        port(portProvider.getPort());
 
+        get("/accounts/:accountId", getGetCreditsRoute(service), GSON::toJson);
+        patch("/accounts/:accountId/unsafeInc", getUnsafeIncRoute(service), GSON::toJson);
+        patch("/accounts/:accountId/inc", getUnsafeIncRoute(service), GSON::toJson);
+    }
+
+    public static Route getGetCreditsRoute(Service service) {
+        return (req, res) -> service.getCredits(req.params("accountId"));
+    }
+
+    public static Route getUnsafeIncRoute(Service service) {
+        return (req, res) -> service.incrementUnsafe(req.params("accountId"), Long.valueOf(req.queryParams("credits")));
+    }
+
+    public static Route getSafeIncRoute(Service service) {
+
+        return (req, res) -> {
+            long amount = Long.valueOf(req.queryParams("amount"));
+            String accountId = req.queryParams("accountId");
+            String transactionId = req.queryParams("transactionId");
+            if(amount == 0)
+                return new Transaction(accountId, TransactionState.CANCELLED, Calendar.getInstance().getTime(), 0);
+            if(transactionId == null){
+                return service.increment(accountId, amount);
+            }else{
+                return service.increment(accountId, amount, transactionId);
+            }
+        };
     }
 }
